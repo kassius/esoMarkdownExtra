9e950c6180f005ea494fcb532ac66d85931b3fe7
kassius <kassiv.s@gmail.com>
2014-08-14 08:28:59 -0300

    initial

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ esotalk-markdownextra
 =====================
 
 Markdown Extra Plugin for esoTalk
+
+It is working but has some bugs, , like those related with `>` (ex., blockquotes) because esoTalk escapes it with `&lgt;` and some related to css.
+
+It is also needed to add some other different css for member controller, for showing members posts.
---
+++ plugin.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!defined("IN_ESOTALK")) exit;
+
+ET::$pluginInfo["MarkdownExtra"] = array(
+    "name"        => "Markdown Extra",
+    "description" => "This plugin uses the Markdown Extra Library from Michel Fortin to render the posts.",
+    "version"     => "1.0",
+    "author"      => "Kassius Iakxos",
+    "authorEmail" => "kassius@users.noreply.github.com",
+    "authorURL"   => "http://github.com/kassius",
+    "license"     => "GPLv2"
+);
+
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
+
+spl_autoload_register(function($class){
+		require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+});
+use \Michelf\MarkdownExtra;
+
+class ETPlugin_MarkdownExtra extends ETPlugin
+{
+	public $md;
+
+	public function handler_format_format($sender)
+	{
+		$sender->content = MarkdownExtra::defaultTransform($sender->content);
+	}
+
+	public function handler_conversationController_renderBefore($sender)
+	{
+		$sender->addCSSFile($this->resource("markdown.css"));
+	}
+	public function handler_memberController_renderBefore($sender)
+	{
+		$sender->addCSSFile($this->resource("markdown.css"));
+	}
+
+
+}
+
+?>
